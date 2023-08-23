32e2e58816223ff995aed81a894d66a941b4d756
AntonBelia <134911799+AntonBelia@users.noreply.github.com>
2023-08-23 15:39:26 +0300

    add_defaultDiv_priveteProperties_tryCatch_removeText

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
-'use strict'
-const work = [
-  'Write code, create magic.', 
-  'Compile your ideas, debug the doubts.', 
-  'Coding: where possibilities become reality.', 
-  'Ctrl + S: Saving progress, saving dreams.',
-   "Solve, code, repeat.", 
-   "Coding fuels innovation.", 
-   "Program your future with keystrokes.", 
-   "Debugging today, coding tomorrow.", 
-   "Code with purpose, impact the world.", 
-   "Unleash your creativity through code."]
+"use strict";
 
 class ImagesGallery {
-  constructor(imagesGalleryWrapper, work) {
-    this.imagesGalleryWrapper = imagesGalleryWrapper;
-    this.work = work;
-  }
-  renderImageCard = (url, work) => {
+  #imagesGalleryWrapper;
+  constructor(imagesGalleryWrapper) {
+    this.#imagesGalleryWrapper = imagesGalleryWrapper !== null ? imagesGalleryWrapper : document.body.appendChild(document.createElement("div"));
+  };
+
+  #renderImageCard = (url) => {
     return `
 		<div class="image-wrapper">
 			<img class="img" src="${url}">
-      <h2>${work}</h2>
 		</div>
 		`;
   };
-  addImageCardsToDOM = (arr) => {
+
+  #addImageCardsToDOM = (arr) => {
     for (let item of arr) {
-		const imageWrapper = document.createElement('div');
-		imageWrapper.innerHTML = this.renderImageCard(item.download_url, work[item.id]);
-		this.imagesGalleryWrapper.appendChild(imageWrapper);
+      const imageWrapper = document.createElement("div");
+      imageWrapper.innerHTML = this.#renderImageCard(item.download_url);
+      this.#imagesGalleryWrapper.appendChild(imageWrapper);
     }
   };
+
   fetchImages = async () => {
-    const response = await fetch(
-      "https://picsum.photos/v2/list?page=1&limit=10");
-    const img = await response.json();
-
-    this.addImageCardsToDOM(img);
+    try {
+      const response = await fetch(
+        "https://picsum.photos/v2/list?page=1&limit=10"
+      );
+      const img = await response.json();
+      this.#addImageCardsToDOM(img);
+    } catch (error) {
+      throw new Error('Data loading error:', error)
+    }
   };
-}
+};
 
-const imagesGalleryWrapper = document.querySelector(".images-gallery");
+(() => {
+  const imagesGalleryWrapper = document.querySelector(".images-gallery");
 
-const imagesGallery = new ImagesGallery(imagesGalleryWrapper, work);
+  const imagesGallery = new ImagesGallery(imagesGalleryWrapper);
 
-imagesGallery.fetchImages();
+  imagesGallery.fetchImages();
+})();
